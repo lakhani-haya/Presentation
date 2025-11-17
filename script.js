dcca3bdce42d5bde30073f87cbd478b17a8717fe
Haya Lakhani <157073204+lakhani-haya@users.noreply.github.com>
2025-11-16 23:43:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class SmoothScrollAnimations {
         this.setupParallaxEffects();
         this.setupTypingEffect();
         
-        // Show first section immediat
+        // Show first section immedia
         if (this.sections.length > 0) {
             this.sections[0].classList.add('visible');
         }
